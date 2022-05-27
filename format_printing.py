ecd684cd790c6d24c73bd56cc8b024ffc03d2a66
DBYang81 <yangdongbin@hotmail.com>
2022-05-27 21:47:40 +0300

    Ex02

--- format_printing.py
+++ format_printing.py
@@ -2,3 +2,8 @@ print("""This is a very big,
 long and even annoying
 command which takes way too much
 space and is irritating to handle.""")
+
+
+print("\n\nThis here is divided to several lines:\n\
+I am still in the same print-command.")
+print("\tName:\tPeter\n\tJob:\tBabysitter")
