01dd1483515285721cfc6c597ed1b70530554b3e
DBYang81 <yangdongbin@hotmail.com>
2022-05-27 21:53:28 +0300

    Ex05

---
+++ string_slice.py
@@ -0,0 +1,7 @@
+var = "desserts"
+fstChar = var[0:4]
+latChar = var[-4:]
+backwards = var[::-1]
+print("The first 4 characters were: " + fstChar)
+print("The last 4 characters were: " + latChar)
+print(f"The string backwards was: {backwards}")
