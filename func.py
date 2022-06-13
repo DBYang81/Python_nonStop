01235b80548257e96639320d55ab531e75e7d835
DBYang81 <yangdongbin@hotmail.com>
2022-06-13 21:54:55 +0300

    Ex Function

---
+++ func.py
@@ -0,0 +1,10 @@
+def main():
+    print("Lets call the subfunction:")
+    hello()
+    print("Quitting.")
+
+def hello():
+    print("Hello there!")
+
+if __name__ == "__main__":
+    main()
---
+++ parameter.py
@@ -0,0 +1,10 @@
+def poweroftwo(power):
+    res = 2**power
+    print("The result was", res)
+
+def main():
+    num = int(input("Give a number: "))
+    poweroftwo(num)
+
+if __name__ == "__main__":
+    main()
